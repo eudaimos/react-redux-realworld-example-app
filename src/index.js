3c21d3c670652635bbbd1bfd9a1c7b1e831e03b4
eudaimos <jeff@hwgray.com>
2019-08-10 01:18:41 -0700

    wire up TAO around app

--- src/index.js
+++ src/index.js
@@ -6,15 +6,31 @@ import { store, history} from './store';
 import { Route, Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
 
+import TAO from '@tao.js/core';
+import { Provider as TaoProvider, DataHandler } from '@tao.js/react';
+
 import App from './components/App';
 
+import initAC from './tao';
+
 ReactDOM.render((
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <Switch>
-        <Route path="/" component={App} />
-      </Switch>
-    </ConnectedRouter>
-  </Provider>
+  <TaoProvider TAO={TAO}>
+    <DataHandler
+      name="appData"
+      term="app" action="init"
+      handler={(tao, data, set, current) => ({ ...current, ...data })}
+      default={{ name: '' }}
+    >
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Switch>
+            <Route path="/" component={App} />
+          </Switch>
+        </ConnectedRouter>
+      </Provider>
+    </DataHandler>
+  </TaoProvider>
 
 ), document.getElementById('root'));
+
+TAO.setAppCtx(initAC);
